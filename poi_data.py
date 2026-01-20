9046162adce742c7c319557ea11c1bb804d97819
cobe44 <id022750379@gmail.com>
2026-01-20 15:36:41 -0300

    Remove comments and clean up POI data structure

--- poi_data.py
+++ poi_data.py
@@ -1,19 +1,14 @@
-# POI Data - Apenas Base de Nuporanga
-# Todas as outras localizações usarão geocodificação reversa para obter nome da cidade
 
-# POIs de Tatui - REMOVIDOS (usará nome da cidade via geocodificação)
 POIS_TATUI = {}
 
-# POIs de Passos - REMOVIDOS (usará nome da cidade via geocodificação)
+
 POIS_PASSOS = {}
 
-# POIs de Ipiguá - REMOVIDOS (usará nome da cidade via geocodificação)
 POIS_IPIGUA = {}
 
-# Base Nuporanga - MANTIDA
+
 POIS_NUPORANGA = {
     "Base Nuporanga (Andrioni)": [(-20.722612, -47.751135)],
 }
 
-# Mapeamento de Cidades - REMOVIDO (não é mais necessário pois usaremos geocodificação)
 MAPA_CIDADES_PASSOS = {}
